4e66dd5102aa2b311f61444865cd7d4f4cec036b
sirakisaias <sirakisaias@gmail.com>
2021-08-12 00:39:06 -0700

    fix

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -9,9 +9,12 @@ const db = require('./scheme-model');
 */
 async function checkSchemeId (req, res, next) {
   try {
-    const schema = await db('schemes'). where('scheme_id', req.params.scheme_id).first()
+    const schema = await db('schemes').where('scheme_id', req.params.scheme_id).first()
     if (!schema) {
-        res.status(404).json({ message: 'schem with scheme_id not found'})
+        next({
+          status: 404,
+          message: `scheme with scheme_id ${req.params.scheme_id} not found`
+        })
     } else {
         next()
     }
